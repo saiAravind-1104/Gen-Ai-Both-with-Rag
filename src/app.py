488414d79117cce1ee216b705cab4d1707686cfe
Saiaravind1104 <108283120+Saiaravind1104@users.noreply.github.com>
2025-10-29 21:48:21 +0530

    code changes

--- src/app.py
+++ src/app.py
@@ -13,23 +13,48 @@ from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 load_dotenv()
 
-st.title("BASIC RAG APPLICATION FOR Q & A")
+os.environ["HF_TOKEN"]=os.getenv("HF_TOKEN")
+os.environ["GROQ_API_KEY"]=os.getenv("GROQ_KEY")
+
+st.title("RAG APPLICATION FOR Q & A")
 st.session_state.message=st.chat_input("please ask your question")
 
 def save_document_in_vectorstore():
-    loader=PyPDFDirectoryLoader("D:\Git Hub\Day1\Gen-Ai-Both-with-Rag\Documents")
+    loader=PyPDFDirectoryLoader("resources\Attention.pdf")
     document=loader.load()
     text_spllit=RecursiveCharacterTextSplitter(chunk_size=200,chunk_overlap=20)
     docs=text_spllit.split_documents(document)
     embeddings=HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
     vectorstore=FAISS.from_documents(docs,embeddings)
-    st.session_state.vector=vectorstore
-    st.chat_message("Message saved in vector store")
+    retriver=vectorstore.as_retriever(search_kwargs={"k": 3})
+    st.session_state.retriver=retriver
+    st.toast("Documents saved in vector")
+    return retriver
 
-if "vector" in st.session_state:
-    output=st.session_state.vector.similarity_search(query=st.session_state.message,k=1)
-    st.chat_message("assistant").write(output[0].page_content)
+prompt = ChatPromptTemplate.from_template("""
+Answer the question based only on the following context:
+
+{context}
+
+Question: {input}
+
+Answer:
+""")
+
+llm=ChatGroq(model="llama-3.1-8b-instant")
+def getLLMOutput(documents):
+    pass
+
+if "retriver" in st.session_state:
+    document_chain=create_stuff_documents_chain(llm=llm,prompt=prompt)
+    retrieval_chain = create_retrieval_chain(st.session_state.retriver, document_chain)
+    result=retrieval_chain.invoke({"input": st.session_state.message})
+    st.chat_message("user").write(st.session_state.message)
+    st.chat_message("assistant").write(result["answer"])
 else:
     save_document_in_vectorstore()
-    output=st.session_state.vector.similarity_search(query=st.session_state.message,k=1)
-    st.chat_message("assistant").write(output[0].page_content)
+    document_chain=create_stuff_documents_chain(llm=llm,prompt=prompt)
+    retrieval_chain = create_retrieval_chain(st.session_state.retriver, document_chain)
+    result=retrieval_chain.invoke({"input": st.session_state.message})
+    st.chat_message("user").write(st.session_state.message)
+    st.chat_message("assistant").write(result["answer"])
